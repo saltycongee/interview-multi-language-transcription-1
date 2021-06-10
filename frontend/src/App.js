480d44e3b3683160ed57e138be59c484fed82ee6
Sathvik Radhakrishnan <sathvik.raki@ubc.ca>
2021-06-09 21:03:16 -0700

    Translation editor 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,6 @@ import "react-dropdown/style.css";
 import axios from "axios";
 import { useAlert } from "react-alert";
 import Dropdown from "react-dropdown";
-import TextField from '@material-ui/core/TextField'
 
 function App(props) {
   // From AWS
@@ -83,11 +82,13 @@ function App(props) {
     translateData: "",
     currentFilename: "",
   });
+
+
   const showEditor = translationEditorStatus.showEditor;
-  const primaryKey = translationEditorStatus.primaryKey;
-  const sortKey = translationEditorStatus.sortKey;
-  const translateData = translationEditorStatus.translateData;
-  const currentFilename = translationEditorStatus.currentFilename;
+  //const primaryKey = translationEditorStatus.primaryKey;
+  //const sortKey = translationEditorStatus.sortKey;
+  const translateData = setTranslationData.translateData;
+  //const currentFilename = translationEditorStatus.currentFilename;
 
   const [jobState, updateJobState] = useState({
     jobs: [],
@@ -254,6 +255,11 @@ function App(props) {
     });
   }
 
+  const handleTranslationChange = (event) => {
+    setTranslationdata(event.target.translateData);
+    console.log(translateData)
+  
+  };
   function showTable() {
     const newRows = jobState.jobs.map((job) => {
       console.log("job");
@@ -439,7 +445,7 @@ function App(props) {
                       <Segment>
                       Editor
                         <Form>
-                          <TextArea placeholder="Translation sample" />
+                          <TextArea value={translateData} onChange={handleTranslationChange} />
                        
                         <Button onClick={portalStatus}>
                           Upload translation
