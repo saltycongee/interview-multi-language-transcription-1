59062000fc657de21c75251665996721509eed72
Sathvik Radhakrishnan <sathvik.raki@ubc.ca>
2021-06-29 02:24:39 -0700

    Comprehend after editing translation 2.3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -495,6 +495,7 @@ function App(props) {
           //Not started
           translateStatus = <Icon name="pause circle" />;
         } else {
+          job.translateKey = translateKey
           translateStatus = (
             <div>
               <Button onClick={() => downloadData(translateKey)}>
