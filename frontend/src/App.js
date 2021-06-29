770b3c2f6c403aa1011948d37686481cbf2a29f3
Sathvik Radhakrishnan <sathvik.raki@ubc.ca>
2021-06-29 01:26:57 -0700

    Comprehend after editing translation 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import {
   Modal,
   Form,
   TextArea,
-  Segment
+  Segment,
 } from "semantic-ui-react";
 import Login from "./Components/Authentication/Login";
 import { Hub } from "aws-amplify";
@@ -42,6 +42,8 @@ function App(props) {
   const scanApi = "https://0opz07581b.execute-api.us-east-1.amazonaws.com/dev";
   const searchApi =
     "https://3wzc0n9cbb.execute-api.us-east-1.amazonaws.com/Stage_1";
+  const updateTranslationApi =
+    "https://ti7db5ed14.execute-api.us-east-1.amazonaws.com/Stage_1";
 
   // Initialisations
 
@@ -86,7 +88,7 @@ function App(props) {
     { value: "en-AB", label: "English (AB)" },
     { value: "en-AU", label: "English (AU)" },
     { value: "en-GB", label: "English (GB)" },
-    
+
     { value: "en-IE", label: "English (IE)" },
     { value: "en-IN", label: "English (IN)" },
     { value: "en-WL", label: "English (WL)" },
@@ -113,7 +115,7 @@ function App(props) {
     { value: "te-IN", label: "Telugu" },
     { value: "tr-TR", label: "Turkish" },
     { value: "zh-CN", label: "Chinese (S)" },
-  ]
+  ];
 
   let file;
   let job_name;
@@ -131,18 +133,16 @@ function App(props) {
     showStatus: false,
   });
 
-
-
   const [showUploadFormStatus, updateUploadFormStatus] = useState({
     showUploadForm: false,
   });
 
-  const [searchedFiles, updateSearchedFiles] = useState([])
-  const [showAllStatus, updateShowAllStatus] = useState(true)
+  const [searchedFiles, updateSearchedFiles] = useState([]);
+  const [showAllStatus, updateShowAllStatus] = useState(true);
 
-  const [maxPerPage, updateMaxPerPage] = useState(2)
-  const [currentPage, updateCurrentPage] = useState(1)
-  const [totalPages, updateTotalPages] = useState(1)
+  const [maxPerPage, updateMaxPerPage] = useState(2);
+  const [currentPage, updateCurrentPage] = useState(1);
+  const [totalPages, updateTotalPages] = useState(1);
 
   const [showKeyphraseSearchStatus, updateKeyphraseSearchStatus] =
     useState(false);
@@ -158,6 +158,10 @@ function App(props) {
 
   const [translationData, updateTranslationData] = useState(" ");
   const [translationKey, updateTranslationKey] = useState("");
+  const [translationKeyUsername, updateTranslationKeyUsername] = useState("");
+  const [translationKeyLanguage, updateTranslationKeyLanguage] = useState("");
+  const [translationKeyFileName, updateTranslationKeyFileName] = useState("");
+
   const [fileStatus, updateFileStatus] = useState(true);
 
   const showEditor = translationEditorStatus.showEditor;
@@ -296,12 +300,10 @@ function App(props) {
       axios
         .post(searchApi, finalSearchPayload)
         .then((response) => {
-          updateSearchedFiles(response['data'])
-          updateShowAllStatus(false)
-          
-          console.log(searchedFiles);
+          updateSearchedFiles(response["data"]);
+          updateShowAllStatus(false);
 
-          
+          console.log(searchedFiles);
         })
         .catch((error) => {
           console.log(error);
@@ -358,14 +360,18 @@ function App(props) {
     a.click();
   }
 
-  async function editTranslation(key) {
-    portalStatus(true);
-    updateTranslationKey(key);
+  async function editTranslation(job) {
+    portalStatus(true); //Open editor
+
+    updateTranslationKey(job.translateKey);
+    updateTranslationKeyUsername(job.username); //Update state for current translation data key
+    updateTranslationKeyLanguage(job.targetLanguage);
+    updateTranslationKeyFileName(job.fileName);
     //console.log(key)
-    const signedURL = await Storage.get(key, {
+    const signedURL = await Storage.get(job.translateKey, {
       download: true,
       cacheControl: "no-cache",
-    });
+    }); //Get txt from S3
     signedURL.Body.text().then((string) => {
       updateTranslationData(string);
     });
@@ -373,37 +379,61 @@ function App(props) {
   }
 
   async function handleTranslationUpload() {
-    const key = translationKey;
-    //console.log(key)
+    const updatePayload = {
+      username: translationKeyUsername,
+      translateTarget: translationKeyLanguage,
+      s3url: translationKey,
+      fileName: translationKeyFileName,
+    };
 
-    const result = await Storage.put(key, translationData, {
+    //console.log(translationKey)
+
+    const result = await Storage.put(translationKey, translationData, {
       progressCallback(progress) {
         console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
       },
     });
-    editTranslation(key);
+    editTranslation(translationKey);
     portalStatus(false);
-    console.log("result from trans upload")
-    console.log(result)
 
+    axios
+      .post(updateTranslationApi, updatePayload)
+      .then((response) => {
+        console.log("response from update");
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    console.log("result from trans upload");
+    console.log(result);
   }
 
   function portalStatus(portalState) {
-
     updateTranslationEditorStatus((prevState) => {
       return { ...prevState, showEditor: portalState };
     });
   }
 
-  function showPanigation(){
-    var returnString = []
-    for (let i = 1; i <= (totalPages+1); i++){
-      returnString.push(<Menu.Item onClick={() => {updateCurrentPage(  i  ); console.log(currentPage)}}>  {i}  </Menu.Item>)
-      }
+  function showPanigation() {
+    var returnString = [];
+    for (let i = 1; i <= totalPages + 1; i++) {
+      returnString.push(
+        <Menu.Item
+          onClick={() => {
+            updateCurrentPage(i);
+            console.log(currentPage);
+          }}
+        >
+          {" "}
+          {i}{" "}
+        </Menu.Item>
+      );
+    }
 
-
-    console.log(returnString)
-    return returnString
+    console.log(returnString);
+    return returnString;
   }
 
   const handleTranslationChange = (event) => {
@@ -417,76 +447,81 @@ function App(props) {
   };
 
   function showTable() {
+    console.log("total");
+    console.log(totalPages);
+    console.log("currentPage");
+    console.log(currentPage);
 
-    console.log('total')
-    console.log(totalPages)
-    console.log('currentPage')
-    console.log(currentPage)
-
-    if (searchedFiles === []){
-    try{
-      updateTotalPages((Math.ceil((jobState.jobs.length)/maxPerPage)))
-    }
-    catch(err){
-      updateTotalPages(1)
-    }
-    
-    console.log('total pages')
-    console.log(totalPages)
-    }
-  
-
-    const newRows = jobState.jobs.filter((job, index) => ((((showAllStatus === true) && (((index/maxPerPage) < currentPage) && ((index/maxPerPage) >= (currentPage -1)))) ) || (searchedFiles.includes(job["fileName"])) )).map((job) => {
-      console.log("job");
-      console.log(job);
-      let tokens = job.transcriptionUrl.split("/").slice(4);
-      const transcribeKey = tokens.join("/");
-      tokens = job.translateKey.split("/").slice(1);
-      const translateKey = tokens.join("/");
-
-      if (job.translateKey === "In progress") {
-        // In progress
-        translateStatus = <Icon loading name="spinner" />;
-      } else if (job.translateKey === "Invalid") {
-        //Not applicable
-        translateStatus = <Icon color="yellow" name="ban" />;
-      } else if (job.translateKey === "Not started") {
-        //Not started
-        translateStatus = <Icon name="pause circle" />;
-      } else {
-        translateStatus = (
-          <div>
-            <Button onClick={() => downloadData(translateKey)}>
-              {" "}
-              <Icon name="download" />{" "}
-            </Button>
-
-            <Button onClick={() => editTranslation(translateKey)}>Edit</Button>
-          </div>
-        );
+    if (searchedFiles === []) {
+      try {
+        updateTotalPages(Math.ceil(jobState.jobs.length / maxPerPage));
+      } catch (err) {
+        updateTotalPages(1);
       }
 
-      return (
-        <Table.Row>
-          <Table.Cell> {job.fileName} </Table.Cell>
-          <Table.Cell>{job.sourceLanguage}</Table.Cell>
-          <Table.Cell>{job.targetLanguage}</Table.Cell>
-          <Table.Cell> {job.status} </Table.Cell>
-          <Table.Cell>
-            {" "}
-            {job.transcriptionUrl !== " " ? (
-              <Button onClick={() => downloadData(transcribeKey)}>
+      console.log("total pages");
+      console.log(totalPages);
+    }
+
+    const newRows = jobState.jobs
+      .filter(
+        (job, index) =>
+          (showAllStatus === true &&
+            index / maxPerPage < currentPage &&
+            index / maxPerPage >= currentPage - 1) ||
+          searchedFiles.includes(job["fileName"])
+      )
+      .map((job) => {
+        console.log("job");
+        console.log(job);
+        let tokens = job.transcriptionUrl.split("/").slice(4);
+        const transcribeKey = tokens.join("/");
+        tokens = job.translateKey.split("/").slice(1);
+        const translateKey = tokens.join("/");
+
+        if (job.translateKey === "In progress") {
+          // In progress
+          translateStatus = <Icon loading name="spinner" />;
+        } else if (job.translateKey === "Invalid") {
+          //Not applicable
+          translateStatus = <Icon color="yellow" name="ban" />;
+        } else if (job.translateKey === "Not started") {
+          //Not started
+          translateStatus = <Icon name="pause circle" />;
+        } else {
+          translateStatus = (
+            <div>
+              <Button onClick={() => downloadData(translateKey)}>
                 {" "}
-                <Icon name="download" />
+                <Icon name="download" />{" "}
               </Button>
-            ) : (
-              <Icon loading name="spinner" />
-            )}
-          </Table.Cell>
-          <Table.Cell> {translateStatus}</Table.Cell>
-        </Table.Row>
-      );
-    });
+
+              <Button onClick={() => editTranslation(job)}>Edit</Button>
+            </div>
+          );
+        }
+
+        return (
+          <Table.Row>
+            <Table.Cell> {job.fileName} </Table.Cell>
+            <Table.Cell>{job.sourceLanguage}</Table.Cell>
+            <Table.Cell>{job.targetLanguage}</Table.Cell>
+            <Table.Cell> {job.status} </Table.Cell>
+            <Table.Cell>
+              {" "}
+              {job.transcriptionUrl !== " " ? (
+                <Button onClick={() => downloadData(transcribeKey)}>
+                  {" "}
+                  <Icon name="download" />
+                </Button>
+              ) : (
+                <Icon loading name="spinner" />
+              )}
+            </Table.Cell>
+            <Table.Cell> {translateStatus}</Table.Cell>
+          </Table.Row>
+        );
+      });
     return newRows;
   }
 
@@ -637,7 +672,13 @@ function App(props) {
                     <Modal open={showEditor}>
                       <Segment>
                         Editor
-                        <Button onClick={() => portalStatus(false)} floated={'right'} circular><Icon name='close'/></Button>
+                        <Button
+                          onClick={() => portalStatus(false)}
+                          floated={"right"}
+                          circular
+                        >
+                          <Icon name="close" />
+                        </Button>
                         <Form>
                           <TextArea
                             value={translationData}
@@ -655,21 +696,26 @@ function App(props) {
                     <Modal open={showKeyphraseSearchStatus}>
                       <Segment>
                         Keyword Search
-                        <Button onClick={() => updateKeyphraseSearchStatus(false)} floated={'right'} circular><Icon name='close'/></Button>
+                        <Button
+                          onClick={() => updateKeyphraseSearchStatus(false)}
+                          floated={"right"}
+                          circular
+                        >
+                          <Icon name="close" />
+                        </Button>
                         <Form>
-                        <Dropdown
-                        options={targetOptions}
-                        onChange={_searchTargetLanguageChosen}
-                        placeholder="Choose from dropdown"
-                        search
-                      />
+                          <Dropdown
+                            options={targetOptions}
+                            onChange={_searchTargetLanguageChosen}
+                            placeholder="Choose from dropdown"
+                            search
+                          />
                           <TextArea
                             placeholder="Enter keyphrase"
                             onChange={handleKeyphraseChange}
                           />
 
                           <Button onClick={searchKeyphrases}>Search</Button>
-
                         </Form>
                       </Segment>
                     </Modal>
