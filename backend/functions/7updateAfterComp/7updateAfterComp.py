f10aae47ed91fe9a10ee0486b9b1c7376410a632
Derrick Cheng <72680915+saltycongee@users.noreply.github.com>
2021-12-03 10:33:58 -0800

    Update 7updateAfterComp.py

--- backend/functions/7updateAfterComp/7updateAfterComp.py
+++ backend/functions/7updateAfterComp/7updateAfterComp.py
@@ -1,5 +1,5 @@
 #To upload filename, language and keyphrases to the corresponding usernames table
-
+import json
 import psycopg2
 import boto3
 
@@ -21,11 +21,19 @@ def lambda_handler(event, context):
     port=port['Parameter']['Value']
     )
     tableName = tableName['Parameter']['Value']
+    event = json.loads(event['body'])
+    event = json.loads(event['input'])
     file_name = event['file_name'] 
 
     uploadData( engine, event['username'], file_name, event['target_language'], event['keyphrases'] ,tableName )
     engine.commit()
     engine.close()  
+    
+    proxy_response = {}
+    proxy_response['statusCode']=200
+    proxy_response["body"] = { 'msg': 'update after comp' }
+
+    return setProxyResponse(proxy_response)
 
 def uploadData( conn, username, file_name, target_language, keyphrases,tableName) :
     cur = conn.cursor()
@@ -35,9 +43,25 @@ def uploadData( conn, username, file_name, target_language, keyphrases,tableName
     if (keyphrases != []):
         cur.execute(updateQuery)
 
-
-    
-
+def setProxyResponse(data):
+        response = {}
+        response["isBase64Encoded"] = False
+        if "statusCode" in data:
+          response["statusCode"] = data["statusCode"]
+        else:
+          response["statusCode"] = 200
+        if "headers" in data:
+            response["headers"] = data["headers"]
+        else:
+            response["headers"] = {
+              'Content-Type': 'application/json', 
+              'Access-Control-Allow-Headers': 'Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token, X-Amz-User-Agent', 
+              'Access-Control-Allow-Origin': '*', 
+              'Access-Control-Allow-Methods': '*',
+              'Access-Control-Allow-Credentials': 'true'
+            } 
+        response["body"] = json.dumps(data["body"])
+        return response
 
    
     
